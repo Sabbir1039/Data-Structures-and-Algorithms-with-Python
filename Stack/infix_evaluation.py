566ca2bdf24cc3ee38749a5cfc14226ca3c20588
Sabbir Hossain <cse.170201039@gmail.com>
2023-10-04 01:43:32 +0600

    infix_evaluation code added

---
+++ Stack/infix_evaluation.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+class InfixEvaluation:
+
+    def __init__(self) -> None:
+        self.stack = deque()
+        self.operatiors = set(["+", "-", "*", "/", "^"])
+        
+    def perform_operation(self, op1, op2, operator):
+        exp = f"{op1} {operator} {op2}"
+        return eval(exp)
+        
+    def infix_evaluation(self, expression):
+        start = len(expression)-1   
+        for i in range(start, -1, -1): # loop through expression in reverse
+            if expression[i].isdigit():
+                self.stack.append(expression[i])
+            elif expression[i] in self.operatiors:
+                op1 = self.stack.pop()
+                op2 = self.stack.pop()
+                result = self.perform_operation(op1, op2, expression[i])
+                self.stack.append(result)
+        return self.stack[-1]
+        
+if __name__ == "__main__":
+    expression = "- + * 5 7 * 4 2 5"
+    
+    infix = InfixEvaluation()
+    result = infix.infix_evaluation(expression)
+    print(result)
