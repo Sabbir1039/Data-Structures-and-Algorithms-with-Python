3bad4aa27234189421ee121c38b366c27541566c
Md. Sabbir Hossain <sabbir.hossain@fintechhub.com.bd>
2025-09-10 22:57:25 +0600

    graph adj_list better implementation added

--- Graph/graph_adj_list_dictionary.py
+++ Graph/graph_adj_list_dictionary.py
@@ -1,35 +1,45 @@
-# Graph (undirected) Adjacency List using dictionary representation in Python 
-# space complexity of this implementation is O(V + E)
-# time complexity of add_edge() is O(1) and print_graph() is O(V+E)
+from collections import defaultdict
 
 class Graph:
-    def __init__(self) -> None:
-        self.adj_list = {}
-    
-    def add_edge(self, s, d) -> None:
-        if s not in self.adj_list:
-            self.adj_list[s] = []
-        if d not in self.adj_list:
-            self.adj_list[d] = []
+    def __init__(self, vertices: list[str]) -> None:
+        if not vertices:
+            raise ValueError("vertices list can not be empty")
+        if len(vertices) != len(set(vertices)):
+            raise ValueError(f"All vertex names must be unique")
         
-        self.adj_list[s].append(d)
-        self.adj_list[d].append(s)
-        
-    def print_graph(self) -> None:
-        for index, list in self.adj_list.items():
-            print(f"Vertex[{index}] : ", end="")
-            for edge in list:
-                print(f" -> {edge}", end="")
-            print('\n')
+        self.vertices_count = len(vertices)
+        self.adj_list = defaultdict(list)
+        self.vertices_set = set(vertices)
+
+
+    def _valid_vertex(self, vertex: str) -> None:
+        if not vertex in self.vertices_set:
+            raise KeyError(f"Vertex '{vertex}' does not exist in the graph")
     
+
+    def add_edge(self, vertex_i: str, vertex_j: str, weight: int = 1) -> None:
+        self._valid_vertex(vertex_i)
+        self._valid_vertex(vertex_j)
+
+        self.adj_list[vertex_i].append((vertex_j, weight))
+        self.adj_list[vertex_j].append((vertex_i, weight))
+    
+    def print_graph(self):
+        print(f"Vertex : Neighbours...")
+        print("=" * 50)
+        for vertex, neighbours in self.adj_list.items():
+            print(f"{vertex} : ", end="")
+            for v in neighbours:
+                print(v, end=" ")
+            print() 
+
 if __name__ == "__main__":
-    graph = Graph()
-    
-    graph.add_edge(0, 1)
-    graph.add_edge(0, 2)
-    graph.add_edge(1, 2)
-    graph.add_edge(1, 3)
-    
-    # print(graph.adj_list)
-    
-    graph.print_graph()
+    nodes = ["A", "B", "C", "D"]
+    g = Graph(nodes)
+
+    g.add_edge("A", "B")
+    g.add_edge("A", "C")
+    g.add_edge("B", "C")
+    g.add_edge("C", "D")
+
+    g.print_graph()
